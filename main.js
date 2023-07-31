30daab262988c1d1255f39b3bc1c1063c84c9e97
Kretki <grigoriotvext@mail.ru>
2023-07-31 05:41:59 +0300

    NicelyStartingArrow
    ChangePapers

--- main.js
+++ main.js
@@ -31,8 +31,9 @@ function resizeOutliner(){
     var width = height/108*164
     document.getElementById("header-liner-u").style.width = width+"px"
     document.getElementById("header-liner-b").style.width = width+"px"
+    document.getElementById("header-bubbles").style.left = width/2+document.getElementById("left-corner-u").getBoundingClientRect().width+"px"
     var bookWidth = width-document.getElementById("canvas").getBoundingClientRect().left
-    document.getElementById("canvas").style.width = bookWidth/2.1+"px"
+    document.getElementById("canvas").style.width = bookWidth/2.1+"px" //podgon
     var left = document.getElementById("canvas").getBoundingClientRect().left
     document.getElementById("canvas").style.height = height+"px"
     document.getElementById("canvas").style.left = bookWidth/2+left+document.getElementById("right-corner-u").getBoundingClientRect().width/1.5+"px"
--- page_flip.js
+++ page_flip.js
@@ -3,11 +3,12 @@ const canvas = document.getElementById("canvas")
 
 const paper1 = document.getElementById("p1")
 const paper2 = document.getElementById("p2")
+const paper3 = document.getElementById("p3")
 
 changePageBtn.addEventListener("click", goNextPage)
 
 let currentLocation = 1
-let numOfPapers = 2
+let numOfPapers = 3
 let maxLocation = numOfPapers+1
 
 function openBook(){
@@ -27,6 +28,10 @@ function goNextPage(){
             case 2:
                 paper2.classList.add("flipped")
                 paper2.style.zIndex = 2
+                break
+            case 3:
+                paper3.classList.add("flipped")
+                paper3.style.zIndex = 3
                 break
         }
         currentLocation++
