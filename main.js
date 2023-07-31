a4ebdbecb997ae8b5f405821436edd11426bd8af
Kretki <grigoriotvext@mail.ru>
2023-07-31 04:37:41 +0300

    ShouldntDoBigBook

--- main.js
+++ main.js
@@ -33,11 +33,9 @@ function resizeOutliner(){
     var bookWidth = width-document.getElementById("canvas").getBoundingClientRect().left
     document.getElementById("canvas").style.width = bookWidth/2.1+"px"
     var left = document.getElementById("canvas").getBoundingClientRect().left
-    console.log(bookWidth, left)
     var height = document.getElementById("canvas").getBoundingClientRect().width/84*108
-    document.getElementById("canvas").style.height = height+"px"
-    document.getElementById("canvas").style.left = bookWidth/2+left+"px"
-    console.log(bookWidth, document.getElementById("canvas").style.left)
+    document.getElementById("canvas").style.height = height-300+"px"
+    // document.getElementById("canvas").style.left = bookWidth/2+left+"px"
     left = 2*document.getElementById("left-corner-u").getBoundingClientRect().width+width
     document.getElementById("right-corner-u").style.left = left+"px"
     document.getElementById("right-corner-b").style.left = left+"px"
