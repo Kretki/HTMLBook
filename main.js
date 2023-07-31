85b13b2d54bace8aa551d95fd0541ebe68d6e231
Kretki <grigoriotvext@mail.ru>
2023-07-31 22:02:12 +0300

    AnimateBoundaries
    But they not fit well

--- main.js
+++ main.js
@@ -26,31 +26,16 @@ function addTextHint(){
     range.insertNode(span)
 }
 
-function resizeOutlinerLeft(){
-    var height = window.screen.height*0.6
-    var width = height/108*164
-    document.getElementById("header-liner-u").style.width = width+"px"
-    document.getElementById("header-liner-b").style.width = width+"px"
-    document.getElementById("header-bubbles").style.left = width/2-2*document.getElementById("left-corner-u").getBoundingClientRect().width+"px"
-    var bookWidth = width-document.getElementById("canvas").getBoundingClientRect().left
-    document.getElementById("canvas").style.width = bookWidth/2.1+"px" //podgon
-    var left = document.getElementById("canvas").getBoundingClientRect().left
-    document.getElementById("canvas").style.height = height+"px"
-    document.getElementById("canvas").style.left = bookWidth/2+left+document.getElementById("right-corner-u").getBoundingClientRect().width/1.5+"px"
-    left = 2*document.getElementById("left-corner-u").getBoundingClientRect().width+width
-    document.getElementById("right-corner-u").style.left = left+"px"
-    document.getElementById("right-corner-b").style.left = left+"px"
-}
 function resizeOutlinerStart(){
     var height = window.screen.height*0.6
     var width = height/108*82
     document.getElementById("header-liner-u").style.width = width+"px"
     document.getElementById("header-liner-b").style.width = width+"px"
-    document.getElementById("header-bubbles").style.left = width/2-2.1*document.getElementById("left-corner-u").getBoundingClientRect().width+"px"
+    document.getElementById("header-bubbles").style.left = width/2-2*document.getElementById("left-corner-u").getBoundingClientRect().width+"px"
     var bookWidth = width-document.getElementById("canvas").getBoundingClientRect().left
     document.getElementById("canvas").style.width = bookWidth+"px" //podgon
     document.getElementById("canvas").style.height = height+"px"
-    document.getElementById("canvas").style.left = document.getElementById("canvas").style.left+47+"px"
+    document.getElementById("canvas").style.left = document.getElementById("canvas").style.left+"px"
     left = 2*document.getElementById("left-corner-u").getBoundingClientRect().width+width
     document.getElementById("right-corner-u").style.left = left+"px"
     document.getElementById("right-corner-b").style.left = left+"px"
@@ -78,6 +63,33 @@ function changeReading(){
     else{
         document.getElementById("set-reading").textContent = "Start reading"
     }
+}
+
+function resizeOutlinerUp(){
+    document.getElementById("header-liner-u").style.transform = "scaleX(2)"
+    document.getElementById("header-liner-b").style.transform = "scaleX(2)"
+    document.getElementById("right-corner-u").style.transform = "translateX("+document.getElementById("header-liner-u").getBoundingClientRect().width/2+"px)"
+    document.getElementById("left-corner-u").style.transform = "translateX("+(-document.getElementById("header-liner-u").getBoundingClientRect().width/2)+"px)"
+    document.getElementById("canvas").style.transform = "translateX("+document.getElementById("header-liner-u").getBoundingClientRect().width/2+"px)"
+    document.getElementById("right-corner-b").style.transform = "translateX("+document.getElementById("header-liner-b").getBoundingClientRect().width/2+"px)"
+    document.getElementById("left-corner-b").style.transform = "translateX("+(-document.getElementById("header-liner-b").getBoundingClientRect().width/2)+"px)"
+}
+
+function resizeOutlinerDown(){
+    document.getElementById("right-corner-u").style.transform = "translateX(0px)"
+    document.getElementById("left-corner-u").style.transform = "translateX(0px)"
+    document.getElementById("right-corner-b").style.transform = "translateX(0px)"
+    document.getElementById("left-corner-b").style.transform = "translateX(0px)"
+    document.getElementById("header-liner-u").style.transform = "scaleX(1)"
+    document.getElementById("header-liner-b").style.transform = "scaleX(1)"
+}
+
+function openBook(){
+    resizeOutlinerUp()
+}
+
+function closeBook(){
+    resizeOutlinerDown()
 }
 
 resizeOutlinerStart()
--- page_flip.js
+++ page_flip.js
@@ -1,6 +1,7 @@
 const nextPageBtn = document.getElementById("right-arrow")
 const prevPageBtn = document.getElementById("left-arrow")
 const canvas = document.getElementById("canvas")
+const mainBlock = document.getElementById("main-block")
 
 const paper1 = document.getElementById("p1")
 const paper2 = document.getElementById("p2")
@@ -13,19 +14,13 @@ let currentLocation = 1
 let numOfPapers = 3
 let maxLocation = numOfPapers+1
 
-function openBook(){
-}
-
-function closeBook(){
-
-}
-
 function goNextPage(){
     if(currentLocation<maxLocation){
         switch(currentLocation){
             case 1:
                 paper1.classList.add("flipped")
                 paper1.style.zIndex = 1
+                openBook()
                 break
             case 2:
                 paper2.classList.add("flipped")
@@ -34,6 +29,8 @@ function goNextPage(){
             case 3:
                 paper3.classList.add("flipped")
                 paper3.style.zIndex = 3
+                closeBook()
+                document.getElementById("canvas").style.transform = "translateX("+document.getElementById("header-liner-u").getBoundingClientRect().width/2+"px)"
                 break
         }
         currentLocation++
@@ -46,6 +43,8 @@ function goPrevPage(){
             case 2:
                 paper1.classList.remove("flipped")
                 paper1.style.zIndex = 4
+                closeBook()
+                document.getElementById("canvas").style.transform = "translateX(0px)"
                 break
             case 3:
                 paper2.classList.remove("flipped")
@@ -54,6 +53,7 @@ function goPrevPage(){
             case 4:
                 paper3.classList.remove("flipped")
                 paper3.style.zIndex = 2
+                openBook()
                 break
         }
         currentLocation--
