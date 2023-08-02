4409e940997e934c3acfce706b4cef0f8a268267
Kretki <grigoriotvext@mail.ru>
2023-08-03 01:10:10 +0300

    MadeContainerForAll

--- main.js
+++ main.js
@@ -45,9 +45,9 @@ function resizeOutlinerStart(){
     document.getElementById("text-bottom-outliner").style.top = (-retValParam("left-corner-u").height+retValParam("header-liner-u").height)+(-retValParam("left-corner-b").height+retValParam("header-liner-b").height+5)+"px"    
     document.getElementById("main-block").style.left = 50+"%"
     document.getElementById("main-block").style.left = retValParam("main-block").left-retValParam("main-block").width/2 + "px"
-    document.getElementById("main-block").style.top = retValParam("main-menu").height+50+"px"
+    // document.getElementById("main-block").style.top = retValParam("main-menu").height+50+"px"
     document.getElementById("block-quotes").style.left = retValParam("main-block").left+retValParam("main-block").width+5+"px"
-    // docuent.getElementById("block-quotes").style.width = retValParam("canvas").width+"px"
+    document.getElementById("block-quotes").style.width = retValParam("canvas").width+60+"px"
 }
 
 function resizeSaved(){
@@ -99,12 +99,16 @@ function openQuotes(){
         for(var i = 0; i<li.length; i++){
             li[i].style.width = retValParam("canvas").width+"px"
         }
+        // document.getElementById("main-block").style.transform = "translateX(-70%)"
+        // document.getElementById("block-quotes").style.transform = "translateX(-70%)"
     }
     else{
         var li = document.getElementById("dropdown-quotes").getElementsByTagName("li")
         for(var i = 0; i<li.length; i++){
             li[i].style.width = "0px"
         }
+        // document.getElementById("main-block").style.transform = "translateX(0%)"
+        // document.getElementById("block-quotes").style.transform = "translateX(0%)"
     }
 }
 
