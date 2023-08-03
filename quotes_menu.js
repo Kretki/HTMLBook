4d3cdb1cca7412c13f0ce44921b1f9050fe9e052
Kretki <grigoriotvext@mail.ru>
2023-08-03 04:31:34 +0300

    BothOpeningWell

--- quotes_menu.js
+++ quotes_menu.js
@@ -1,21 +1,51 @@
 const quotesWindow = document.getElementById("quotes")
+const blockSchemeWindow = document.getElementById("block-scheme")
 const dropdownNotes = document.getElementById("dropdown-notes")
 const blockNotes = document.getElementById("block-notes")
 
 blockNotes.style.top = document.getElementById("header-liner-u").getBoundingClientRect().height+3+"px"
 dropdownNotes.style.height = document.getElementById("canvas").getBoundingClientRect().height + "px"
 
-function openQuotes(){
-    if(quotesWindow.getBoundingClientRect().height<30){
-        quotesWindow.style.height = document.getElementById("canvas").getBoundingClientRect().height - 30 + "px"
-        quotesWindow.style.alignItems = "end"
-        quotesWindow.style.paddingBottom = "5px"
+var openedWindows = []
+
+function openWindow(element){
+    if(openedWindows.length == 0){
+        element.style.height = document.getElementById("canvas").getBoundingClientRect().height - 30 + "px"
+        element.style.alignItems = "end"
+        element.style.paddingBottom = "5px"
+        openedWindows.push(element)
     }
     else{
-        quotesWindow.style.height = "25px"
-        quotesWindow.style.alignItems = "center"
-        quotesWindow.style.paddingBottom = "0px"
+        if(openedWindows.length == 1){
+            if(openedWindows[0] == element){
+                element.style.height = "25px"
+                element.style.alignItems = "center"
+                element.style.paddingBottom = "0px"
+                openedWindows.splice(openedWindows.indexOf(element), 1)
+            }
+            else{
+                quotesWindow.style.height = document.getElementById("canvas").getBoundingClientRect().height/2 + "px"
+                quotesWindow.style.alignItems = "end"
+                quotesWindow.style.paddingBottom = "5px"
+                blockSchemeWindow.style.height = document.getElementById("canvas").getBoundingClientRect().height/2 + "px"
+                blockSchemeWindow.style.alignItems = "end"
+                blockSchemeWindow.style.paddingBottom = "5px"
+                openedWindows.push(element)
+            }
+        }
+        else{
+            openedWindows.splice(openedWindows.indexOf(element), 1)
+            element.style.height = "25px"
+            element.style.alignItems = "center"
+            element.style.paddingBottom = "0px"
+            openedWindows[0].style.height = document.getElementById("canvas").getBoundingClientRect().height - 30 + "px"
+        }
     }
 }
 
-quotesWindow.onclick = openQuotes
+quotesWindow.onclick = function(){
+    openWindow(quotesWindow)
+}
+blockSchemeWindow.onclick = function(){
+    openWindow(blockSchemeWindow)
+}
