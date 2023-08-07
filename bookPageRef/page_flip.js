984012d27112bf68b3e7c9024af7f9fc6ad53ff0
Kretki <grigoriotvext@mail.ru>
2023-08-07 11:30:25 +0300

    SomethingStrangeWithOpeningBook

--- bookPageRef/page_flip.js
+++ bookPageRef/page_flip.js
@@ -6,8 +6,26 @@ const mainBlock = document.getElementById("main-block")
 const paper1 = document.getElementById("p1")
 const paper2 = document.getElementById("p2")
 const paper3 = document.getElementById("p3")
+const frontH1 = document.getElementById("front-h1")
+const frontH3 = document.getElementById("front-h3")
+const backP1 = document.getElementById("back1-p")
+const allFront = document.getElementsByClassName("front")
+const allBack = document.getElementsByClassName("back")
 
-const textData = document.getElementById("text-for-book")
+const liTextData = document.getElementById("text-for-book").getElementsByTagName("li")
+for(var i = 0; i<liTextData.length; i++){
+    var spli = liTextData[i].textContent.split(":")
+    if(spli[0] == "author"){
+        frontH3.innerHTML = spli[1]
+    }
+    if(spli[0] == "book-title"){
+        frontH1.innerHTML = spli[1]
+    }
+    if(spli[0] == "annotation"){
+        backP1.innerHTML = spli[1]
+    }
+}
+
 
 nextPageBtn.addEventListener("click", goNextPage)
 prevPageBtn.addEventListener("click", goPrevPage)
@@ -16,21 +34,52 @@ let currentLocation = 1
 let numOfPapers = 3
 let maxLocation = numOfPapers+1
 
+function disableTransition(){
+    for(var i = 0; i<allFront.length; i++){
+        allFront[i].classList.add('notransition')
+        allBack[i].classList.add('notransition')
+    }
+}
+
+function enableTransition(){
+    for(var i = 0; i<allFront.length; i++){
+        allFront[i].classList.remove('notransition')
+        allBack[i].classList.remove('notransition')
+    }
+}
+
+function skipToPrevPage(){
+    currentLocation--
+    setTimeout(()=>{
+        disableTransition()
+        paper2.classList.remove("flipped")
+        paper2.style.zIndex = 3
+        }, 600)
+    enableTransition()
+}
+
 function goNextPage(){
     if(currentLocation<maxLocation){
         switch(currentLocation){
             case 1:
                 paper1.classList.add("flipped")
-                paper1.style.zIndex = 1
+                setTimeout(()=>{
+                    paper1.style.zIndex = 1
+                }, 500)
                 openBook()
                 break
             case 2:
                 paper2.classList.add("flipped")
-                paper2.style.zIndex = 2
+                setTimeout(()=>{
+                    paper2.style.zIndex = 2
+                }, 500)
+                // skipToPrevPage()
                 break
             case 3:
                 paper3.classList.add("flipped")
-                paper3.style.zIndex = 3
+                setTimeout(()=>{
+                    paper3.style.zIndex = 3
+                }, 500)
                 closeBook()
                 document.getElementById("canvas").style.transform = "translateX("+(document.getElementById("header-liner-u").getBoundingClientRect().width/2-5)+"px)"
                 break
--- parse.py
+++ parse.py
@@ -6,28 +6,46 @@ root = tree.getroot()
 prefix = root.tag[:root.tag.find("}")+1] 
 desc = root.find(prefix+"description").find(prefix+"title-info")
 author = desc.find(prefix+"author")
+book_title = desc.find(prefix+"book-title")
 ann = desc.find(prefix+"annotation")
+annot = ""
+for text in ann.findall(prefix+"p"):
+    annot += text.text.replace("\xa0", " ") + " "
 body = root.find(prefix+"body")
-authors = [author.find(prefix+"first-name").text, author.find(prefix+"last-name").text]
-authors = [au for au in authors if au is not None]
-deep = {"author":authors, "annotation": [text.text.replace("\xa0", " ") for text in ann.findall(prefix+"p")]}
-title = []
-text = []
+authors = ""
+if(author.find(prefix+"first-name").text is not None):
+    authors += author.find(prefix+"first-name").text + " "
+if(author.find(prefix+"last-name").text is not None):
+    authors += author.find(prefix+"last-name").text
+deep = {"author":authors, "book-title":[book_title.text], "annotation": annot}
+title = ""
+text = ""
 title_count = 0
 text_count = 0
+rest_p = 0
 for elem in body.iter():
     if elem.tag == prefix+"title":
-        if(text != []):
+        if(text != ""):
             deep[f"text-{text_count}"] = text
-        text = []
+        text = ""
         text_count += 1
-        title += [text.text.replace("\xa0", " ") for text in elem.findall(prefix+"p")]
+        for tex in elem.findall(prefix+"p"):
+            title += tex.text.replace("\xa0", " ")+" "
+        rest_p = len(elem.findall(prefix+"p"))
     if elem.tag == prefix+"p":
-        if(title != []):
+        if rest_p != 0:
+            rest_p -= 1
+            if(rest_p == 0):
+                if(title != ""):
+                    deep[f"titile-{title_count}"] = title
+                    title = ""
+                    title_count += 1
+            continue
+        if(title != ""):
             deep[f"titile-{title_count}"] = title
-        title = []
-        title_count += 1
-        text += [elem.text.replace("\xa0", " ")]
-if(text != []):
+            title = ""
+            title_count += 1
+        text += elem.text.replace("\xa0", " ")+" "
+if(text != ""):
     deep[f"text-{text_count}"] = text
 print(json.dumps(deep))
